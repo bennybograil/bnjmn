8c9e3a75c86c964f1f3f28b6f4ce4c93f5a6ab7b
ben <benjaminwiki99@gmail.com>
2026-03-31 19:31:05 +1300

    ksadks

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("get the fuck out of my dev tools you thief!!!")
 
-const backend = fetch(http://127.0.0.1:8000/)
+const url = "http://127.0.0.1:8000/"
+const backend = fetch(url)
 console.log(backend)

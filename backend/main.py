f9b5fc4232f0aacdf8deb516053d2b30ead3f91e
ben <benjaminwiki99@gmail.com>
2026-04-01 12:52:01 +1300

    add server

--- backend/main.py
+++ backend/main.py
@@ -2,23 +2,19 @@ from pathlib import Path
 import sys
 
 from fastapi import FastAPI
+from cors import setup_cors
+
 
 from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app):
-    app.add_middleware(
+   app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
-CURRENT_DIR = Path(__file__).resolve().parent
-if str(CURRENT_DIR) not in sys.path:
-    sys.path.append(str(CURRENT_DIR))
-
-from cors import setup_cors
 
 app = FastAPI()
 setup_cors(app)

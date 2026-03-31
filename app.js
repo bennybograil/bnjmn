74dfcbcfd29849d6706f13aeedbf135ccc344c07
ben <benjaminwiki99@gmail.com>
2026-04-01 12:41:39 +1300

    add server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("get the fuck out of my dev tools!!!")
 
 async function run() {
-  const res = await fetch("https://your-url")
+  const res = await fetch("https://circulation-claims-appliance-assumed.trycloudflare.com")
   const data = await res.text()
   console.log(data)
 }
